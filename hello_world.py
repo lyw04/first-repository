b12917edc0bfe9ae877fc5055a311277f681b276
lyw04 <sulrang04@naver.com>
2023-09-14 11:08:06 +0900

    Create hello_world.py

---
+++ hello_world.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Sep 14 11:06:01 2023
+
+@author: LYW
+"""
+
+def print_hello():
+    print("Hello World!!")
+
+if __name__ == '__main__':
+    print_hello()
