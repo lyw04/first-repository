6f3f4d8af1ca3b22decc774625dca5c519aa0cba
lyw04 <sulrang04@naver.com>
2023-09-14 11:10:50 +0900

    Update hello_world.py

--- hello_world.py
+++ hello_world.py
@@ -8,5 +8,9 @@ Created on Thu Sep 14 11:06:01 2023
 def print_hello():
     print("Hello World!!")
 
+def print_KIT():
+    print("Kumoh National Institute of Technology.")
+    
 if __name__ == '__main__':
+    print_KIT()
     print_hello()
